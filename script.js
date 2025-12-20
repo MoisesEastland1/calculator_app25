4f7120ab0aa2dce3ad5a9ef2b6def1bcc34e667e
MoisesEastland1 <meastland977@gmail.com>
2025-12-20 09:40:20 -0600

    made some changes

---
+++ script.js
@@ -0,0 +1,14 @@
+const btnVal = [
+"7","8","9","-","4","5","6","x","1","2","3","/","0",".","="
+]
+
+const display = document.getElementById("display");
+const plusBtn = document.getElementById("plus-btn");
+
+const plusValue = "+";
+
+plusBtn.id = `${plusValue} ${plusBtn}`;
+console.log(plusBtn.id);
+function displayInput(input) {
+  
+}
